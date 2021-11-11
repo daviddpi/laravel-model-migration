342f20a622675d23fdbfcaaaf6cf5ce30e10f751
daviddpi <piscopodavid97@gmail.com>
2021-11-11 17:23:57 +0100

    sistemata parte grafica col-4 dei travels

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-4">
-                <h1>Travels</h1>
-                @forelse ($travels as $travel)
+             
+            <h1>Travels</h1>
+            @forelse ($travels as $travel)
+                <div class="col-4">
                     <figure>
                         <img class="img-fluid" src="{{$travel->url_place}}" alt="{{$travel->place}}">
                     </figure>
@@ -13,10 +14,11 @@
                     <h3>{{$travel->country}}</h3>
                     <p>{{$travel->description}}</p>
                     <h6>{{$travel->price}}</h6>
-                @empty
-                    <h2>Non c'è nessun risultato</h2>
-                @endforelse
-            </div>
+                </div>
+            @empty
+                <h2>Non c'è nessun risultato</h2>
+            @endforelse
+            
         </div>
     </div>
 @endsection
